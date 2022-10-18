2205006f8174828657397907e04950786debf481
unknown <andre.erminio@gmail.com>
2022-10-18 16:59:33 -0300

    Subindo o arquivo .env

--- app/Controllers/Http/CidadesController.ts
+++ app/Controllers/Http/CidadesController.ts
@@ -3,16 +3,18 @@ import Cidade from 'App/Models/Cidade'
 
 export default class CidadesController {
     public async index({ response }: HttpContextContract) {
+        /*
         const cidades = await Cidade.query().whereHas("estabelecimentos", (query) => {
             query.where("bloqueado", false);
         }).preload("estado");
+        */
 
-        return response.ok(cidades);
+        return response.ok('Teste');
     }
 
     public async Estabelecimentos({ params, response }: HttpContextContract) {
         const cidade = await Cidade.query().where("id", params.id).preload("estabelecimentos").firstOrFail();
-
+        
         return response.ok(cidade.estabelecimentos);
     }
 }
